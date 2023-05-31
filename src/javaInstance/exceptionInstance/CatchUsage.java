9c4e17f1fea40246e5e0f2a1381af90834a9f4a3
关健强 <905685941@qq.com>
2023-05-31 14:03:43 +0800

    feature: ExceptionOperate

---
+++ src/javaInstance/exceptionInstance/CatchUsage.java
@@ -0,0 +1,13 @@
+package javaInstance.exceptionInstance;
+
+public class CatchUsage {
+    // Catch处理异常
+    public static void main(String[] args) {
+        try{
+            int[]arr = new int[]{1,2,3};
+            System.out.println(arr[4]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/javaInstance/exceptionInstance/init.java
+++
@@ -1,4 +0,0 @@
-package javaInstance.exceptionInstance;
-
-public class init {
-}
